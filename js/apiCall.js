7c2873d5fa9b3cde3b8653653ac1673731ec6d16
Salgado2004 <leosalgado2004@gmail.com>
2023-01-19 17:08:32 -0300

    Tirei os modelos estáticos de notícia

--- js/apiCall.js
+++ js/apiCall.js
@@ -3,23 +3,36 @@ const categoria = document.querySelectorAll(".category");
 function callNoticias(categoria){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function (){
+        let principal = document.querySelector(".main .image");
         let noticias = document.querySelector(".see-more");
         let resposta = JSON.parse(this.response);
 
-        resposta.data.forEach(element => {
+        resposta.data.forEach((element, index) => {
         let noticia = element;
-        noticias.innerHTML += `
-                <hr>
-                <div class="news">
-                    <img src="${noticia.imageUrl}">
+        if (index == 0){
+            principal.innerHTML = `
+                <div class="image">
+                    <img src="${noticia.imageUrl}" alt="imagem">
                     <div class="typo">
-                        <h3 id="nome-noticia">${noticia.title}</h3>
-                        <p  class="description" maxlength="80">${noticia.content}<span>... <a href="#" class="read-more">Read more</a></span></p>
-                        <p>Published by <span id="nome-autor">${noticia.author}</span></p>
-                        <p class="time">${noticia.date} ${noticia.time}</p>
+                        <div class="tag-novo">${resposta.category}</div>
+                        <h2 id="nome-noticia">${noticia.title}</h2>
+                        <p>Publicado por <span id="nome-autor">${noticia.author}</span></p>
                     </div>
                 </div>
-        `;
+            `;
+        } else{
+            noticias.innerHTML += `
+            <div class="news">
+                <img src="${noticia.imageUrl}">
+                <div class="typo">
+                    <h3 id="nome-noticia">${noticia.title}</h3>
+                    <p  class="description" maxlength="80">${noticia.content}<span>... <a href="#" class="read-more">Read more</a></span></p>
+                    <p>Published by <span id="nome-autor">${noticia.author}</span></p>
+                    <p class="time">${noticia.date} ${noticia.time}</p>
+                </div>
+            </div>
+            <hr>`;
+        }
         }); 
     }
     xhttp.open("GET", `https://inshorts.deta.dev/news?category=${categoria}`);
@@ -34,3 +47,7 @@ categoria.forEach(element => {
     }
 });
 
+document.body.onload = function(e){
+    alert("chamou");
+    callNoticias("all");
+};
